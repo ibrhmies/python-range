62368ec95100d0fb7599766b954dfa530270f6cb
ibrhmies <02220201044@ogr.inonu.edu.tr>
2022-11-13 14:22:10 +0300

    range
    python range komutuyla for döngüsü

---
+++ range.py
@@ -0,0 +1,8 @@
+for i in range(10):      #range komutuyla sadece bir sayı yazarsak 0 dan başlar o sayıya kadar ekrana yazdırır.
+    print(i)
+
+# range(başlangıç sayısı,bitiş sayısı,artış miktarı ) şeklinde tanımlayabiliriz.
+
+for i in range(10,140):
+    if (i % 2 == 0):
+        print(i)    
